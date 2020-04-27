8254a037d7c6a6e463f07c3503d4bd8dc77cb067
Brandon Blaschke <brandonj.blaschke@gmail.com>
2020-04-26 20:49:39 -0700

    Fixed removing highlights from file & fixed remove all command

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-class MarkedEditor {
-	filePath: string;
-	ranges: vscode.Range[];
-	constructor(filePath: string) {
-		this.filePath = filePath;
-		this.ranges = [];
-	}
-}
+// TODO: Remove all console.log statements
 
-const markedEditors = new Map<string, vscode.Range[]>();
+let activeEditor: vscode.TextEditor | undefined = undefined;
+const defaultColor = '#fdff322f';
 
-// Decorator type to highlight
-const highlightDecorator = vscode.window.createTextEditorDecorationType({
-	backgroundColor: '#fdff322f',
-});
-
-const noHighlightDecorator = vscode.window.createTextEditorDecorationType({
-	backgroundColor: '#fdff3200'
-});
+// TODO: File keys shouldn't be based on URI incase files change location
+// Editors that have been marked
+const markedEditors = new Map<string, Map<string, {ranges: vscode.Range[], decoration: vscode.TextEditorDecorationType}>>();
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -40,7 +29,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// vscode.window.showInformationMessage('Easy Highlight!');
 		
 		// Get current text editor that is open and their selection
-		let activeEditor = vscode.window.activeTextEditor;
+		activeEditor = vscode.window.activeTextEditor;
 		if (!activeEditor) {
 			return;
 		}
@@ -54,40 +43,111 @@ export function activate(context: vscode.ExtensionContext) {
 		let startPos = new vscode.Position(start.line, start.character);
 		let endPos = new vscode.Position(end.line, end.character);
 		let range = new vscode.Range(startPos, endPos);
-		if (markedEditors.has(uri)) {
-			markedEditors.get(uri)?.push(range);
-		} else {
-			markedEditors.set(uri, [range]);
+		
+		// Create range key
+		const rangeKey = `${start.line}${start.character}${end.line}${end.character}`;
+
+		if (!markedEditors.has(uri)) {
+			markedEditors.set(uri, new Map<string, {ranges: vscode.Range[], decoration: vscode.TextEditorDecorationType}>());
 		}
 
-		let temp: vscode.Range[] =  markedEditors.get(uri)!;
-		console.log(temp);
-		activeEditor.setDecorations(highlightDecorator, temp);
+		const rangeMap = markedEditors.get(uri)!;
+
+		const decoration = vscode.window.createTextEditorDecorationType({
+			backgroundColor: defaultColor,
+		});
+
+		// If range already exist update decoration else add new one
+		if (rangeMap.has(rangeKey)) {
+			let hlObject = rangeMap.get(rangeKey)!;
+			hlObject.decoration = decoration;
+		} else {
+			rangeMap.set(rangeKey, {ranges: [range], decoration});
+		}
+
+		updateDecorations(activeEditor);
 	});
 
+	// TODO: Dispose of the highlight that matches that line and spice it or something
 	let disposableNoHighlight = vscode.commands.registerCommand("easy-highlight.RemoveHighlight", () => {
 		
 		console.log('Remove Highlight');
 
 		// Get current text editor that is open and their selection
-		let activeEditor = vscode.window.activeTextEditor;
+		// activeEditor = vscode.window.activeTextEditor;
+		// if (!activeEditor) {
+		// 	return;
+		// }
+
+		// const uri = activeEditor.document.uri.toString();
+		// let start = activeEditor.selection.start;
+		// let end = activeEditor.selection.end;
+
+		// // Create range
+		// let startPos = new vscode.Position(start.line, start.character);
+		// let endPos = new vscode.Position(end.line, end.character);
+		// let range = new vscode.Range(startPos, endPos);
+
+		// if (markedEditors.has(uri)) {
+		// 	markedEditors.get(uri)?.push(range);
+		// } else {
+		// 	markedEditors.set(uri, [range]);
+		// }
+
+		// updateDecorations(noHighlightDecorator);
+	});
+
+	// Disposes of all highlights on a single text editor
+	let disposableRemoveAll = vscode.commands.registerCommand("easy-highlight.RemoveAllHighlights", () => {
+		console.log("Remove all highlights");
+
 		if (!activeEditor) {
 			return;
 		}
 
-		let start = activeEditor.selection.start;
-		let end = activeEditor.selection.end;
+		const uri = activeEditor.document.uri.toString();
 
-		// Create range
-		let startPos = new vscode.Position(start.line, start.character);
-		let endPos = new vscode.Position(end.line, end.character);
-		let range = new vscode.Range(startPos, endPos);
+		if (!markedEditors.has(uri)) {
+			return; 
+		}
 
-		// activeEditor.setDecorations(noHighlightDecorator, ranges);
+		markedEditors.get(uri)?.forEach((val, key) => {
+			val.decoration.dispose();
+			val.ranges = [];
+		});
 	});
+
+	// Updates Decorations on current ActiveEditor
+	let updateDecorations = (activeEditor: vscode.TextEditor) => {
+
+		const uri = activeEditor.document.uri.toString();
+		if (!markedEditors.has(uri)) {
+			return;
+		}
+
+		markedEditors.get(uri)?.forEach((val, key) => {
+			activeEditor.setDecorations(val.decoration!, val.ranges!);
+		});
+	};
+
+	// When editor switches udpate activeEditor and udpate decorations.
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			updateDecorations(editor);
+		}
+	}, null, context.subscriptions);
+
+	// When editor switches text documents update ActiveEditor and decorations.
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			updateDecorations(activeEditor);
+		}
+	}, null, context.subscriptions);
 
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposableNoHighlight);
+	context.subscriptions.push(disposableRemoveAll);
 }
 
 // this method is called when your extension is deactivated
